99b61f248ce19fd552119a067e2df34640512b0b
Enes Sağıroğlu <enes-2001sagroglu@hotmail.com>
2024-02-22 22:34:15 +0300

    fx

--- Project/coordinates.py
+++ Project/coordinates.py
@@ -2,11 +2,13 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
 import time
 
 
-street_names_test = ["RESUL SK. tepeustu","LIDER SK. tepeustu"]
+street_names = []
 coordinates = []
+neigbourhood_and_street = {}
 
 point_path = ''
 coordinat_path = '/html/body/div[1]/div[2]/div[10]/div[1]/div[1]/div[1]/div/div[1]/div/div/div[3]/div[2]/span[2]/div/div/div/div[1]'
@@ -21,10 +23,10 @@ def get_coordinates(url, street_names):
 
     for street in street_names:
     # Arama kutusunu bul ve sokak adını gir
-        arama_kutusu = driver.find_element(By.XPATH, '//*[@id="b9663a41-358f-43b5-a560-69e61e93afdc"]')
-        arama_kutusu.clear()
+        arama_kutusu = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[2]/div[3]/header/div/div/div/form/div[1]/div/span/span/input')
+        
         arama_kutusu.send_keys(street)
-        driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[3]/header/div/div/div/form/div[2]/button').click()
+        driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[2]/div[3]/header/div/div/div/form/div[2]/button').click()
         
         # Sonuçları bekleyin
         time.sleep(5)  # Uygun bir bekleme süresi ayarlayabilirsiniz
@@ -34,6 +36,7 @@ def get_coordinates(url, street_names):
             
             coordinat_data = driver.find_element(by= By.XPATH, value= coordinat_path)
             coordinates.append(coordinat_data.text)
+            driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[2]/div[3]/header/div/div/div/form/div[4]/button').click()
             
            
             print(coordinates)
@@ -45,7 +48,19 @@ def get_coordinates(url, street_names):
     # Close the driver
     driver.quit()
 
-get_coordinates("https://yandex.com.tr/maps", street_names_test)
+def get_streets_csv():
+    df = pd.read_csv("./data/streets.csv")
+    #column names are neigborhood, street. convert them to a dictionary
+    neigbourhood_and_street = df.to_dict()
+    print(neigbourhood_and_street)
+
+
+def get_coordinates_to_csv():
+    df = pd.DataFrame(coordinates, columns=["coordinates"])
+    df.to_csv("./data/coordinates.csv", index=False)
+
+get_streets_csv()
+# get_coordinates("https://yandex.com.tr/maps", street_names_test)
 
     
  
