5f0646aa6444912d30a4dee5c3af863961a67a33
Utku Baygüven <79966502+utkubayguven@users.noreply.github.com>
2024-05-01 00:52:40 +0300

    added create map

--- Project/data/create_map.py
+++ Project/data/create_map.py
@@ -1,6 +1,5 @@
 import folium
 import json
-
 # Create a map centered around the average coordinates
 latitude = (41.0189 + 40.9382 + 40.7852) / 3
 longitude = (29.1275 + 29.1557 + 29.4961) / 3
@@ -15,31 +14,22 @@ folium.Marker([41.079010, 29.257734], popup='Reşadiye').add_to(map_istanbul)
 
 def add_points_from_json(file_name, map_object):
     file_path = file_name
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        
-        for item in data:
-            try:
-                # Ensure latitude and longitude are correctly ordered
-                latitude = float(item["latitude"].strip().strip('"'))
-                longitude = float(item["longitude"].strip().strip('"'))
-                popup_info = (f"Name: {item['name']}<br>"
-                              f"Population: {item['population']}<br>"
-                              f"Population Rate: {item['rate_of_population']}<br>"
-                              f"Demand: {item['demand']}")
-                # Correct order of latitude and longitude for the marker
-                folium.Marker([latitude, longitude], popup=folium.Popup(popup_info, max_width=250)).add_to(map_object)
-                print(f"Added marker for {item['name']} at ({latitude}, {longitude})")
-            except ValueError as e:
-                print(f"Error converting coordinates for {item['name']}: {e}")
-    except FileNotFoundError as e:
-        print(f"File not found: {e}")
-    except Exception as e:
-        print(f"An error occurred while processing {file_name}: {e}")
+    with open(file_path, 'r', encoding='utf-8') as file:  # Ensure using utf-8 encoding
+        data = json.load(file)
+    
+    for item in data:
+        try:
+            # Remove any extraneous whitespace or quotation marks
+            latitude = float(item["longitude"].strip().strip('"'))
+            longitude = float(item["latitude"].strip().strip('"'))
 
-
-
+            popup_info = (f"Name: {item['name']}<br>"
+                          f"Population: {item['population']}<br>"
+                          f"Population Rate: {item['rate_of_population']}<br>"
+                          f"Demand: {item['demand']}")
+            folium.Marker([latitude, longitude], popup=folium.Popup(popup_info, max_width=250)).add_to(map_object)
+        except ValueError as e:
+            print(f"Error converting coordinates for {item['name']}: {e}")
 
 
 # Load data from JSON files and add to map
@@ -48,7 +38,7 @@ add_points_from_json('basibuyuk.json', map_istanbul)
 add_points_from_json('tepeustu.json', map_istanbul)
 
 # Save the map as an HTML file
-map_istanbul.save('combined_istanbul_neighborhoods.html')
+map_istanbul.save('neighborhoods.html')
 
 
 
