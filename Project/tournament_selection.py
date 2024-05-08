8e08d5da32c2f0c2927161b0c4fe6611f6dcd76c
Enes Sağıroğlu <enes-2001sagroglu@hotmail.com>
2024-05-08 22:54:57 +0300

    ilk hali

---
+++ Project/tournament_selection.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+
+def tournament_selection(population, fitness_scores, tournament_size=4):
+    """Selects two parents from the population using tournament selection."""
+    # Turnuva için rastgele bireyler seç
+    participants = random.sample(list(enumerate(population)), tournament_size)
+    
+    # Participants listesinden (index, chromosome) çiftleri olarak alınır
+    # Fitness skorlarına göre sıralayarak en iyi iki bireyi bul
+    sorted_participants = sorted(participants, key=lambda x: fitness_scores[x[0]], reverse=True)
+    
+    # En iyi iki bireyi dön
+    return [population[idx] for idx, _ in sorted_participants[:2]]
+
